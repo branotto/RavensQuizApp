c1d879265e6b4e16c8503435c941945281b72b07
branotto <branaotto@gmail.com>
2018-02-19 23:19:33 -0500

    Add files via upload

---
+++ Data.js
@@ -0,0 +1,99 @@
+'use strict';
+
+const questionBank = 
+[
+  { question : "In what year was the Raven's inaugural season?",
+    answer_one: 1996,
+    answer_two: 1986,
+    answer_three: 1990,
+    answer_four: 2001,
+    correct_answer: '1996'
+  },
+  
+  { question : "Who were the Ravens first 2 draft picks?",
+    answer_one: 'Peter Boulware and Jamie Sharper',
+    answer_two: 'Duane Starks and Jim Harbaugh',
+    answer_three: 'Chris McAlister and Tony Banks',
+    answer_four: 'Jonathon Ogden and Ray Lewis',
+    correct_answer: 'Jonathon Ogden and Ray Lewis'
+  },
+  
+  { question : "What was the first stadium the Ravens played in in Baltimore?",
+    answer_one: 'M&T Bank Stadium',
+    answer_two: 'Oriole Park at Camden Yards',
+    answer_three: 'Memorial Stadium',
+    answer_four: 'PSI Net Stadium',
+    correct_answer: 'Memorial Stadium'
+  },
+
+  { question : "How many Head Coaches have the Ravens had?",
+    answer_one: 5,
+    answer_two: 3,
+    answer_three: 2,
+    answer_four: 4,
+    correct_answer: '3'
+  },
+  
+  { question : "Which 2 Super Bowls did the Ravens win?",
+    answer_one: 'XXXV and XLVII',
+    answer_two: 'XXX and XXIX',
+    answer_three: 'XXXVI and XL',
+    answer_four: 'XLV and LI',
+    correct_answer: 'XXXV and XLVII'
+  },
+  
+  { question : "Who is the Ravens leader in career touchdowns for a non QB?",
+    answer_one: 'Todd Heap',
+    answer_two: 'Torrey Smith',
+    answer_three: 'Anquan Boldin',
+    answer_four: 'Jamal Lewis',
+    correct_answer: 'Jamal Lewis'
+  },
+  
+  { question : "What is the Ravens best single season record?",
+    answer_one: '15 - 1',
+    answer_two: '10 - 6',
+    answer_three: '13 - 3',
+    answer_four: '11 - 5',
+    correct_answer: '13 - 3'
+  },
+  
+  { question : "The Ravens current owner is Steve Bisciotti. Who was the previous owner?",
+    answer_one: 'Peter Angelos',
+    answer_two: 'Art Modell',
+    answer_three: 'Jim Irsay',
+    answer_four: 'Jack Kent Cooke',
+    correct_answer: 'Art Modell'
+  },
+  
+  { question : "Which former NFL player has served as the Ravens General Manager?",
+    answer_one: 'Ozzie Newsome',
+    answer_two: 'John Elway',
+    answer_three: 'Bill Belichick',
+    answer_four: 'Jon Lynch',
+    correct_answer: 'Ozzie Newsome'
+  },
+
+  { question : "During John Harbaugh's tenure as Head Coach, how many of his assistant coaches have become the Head Coach of a NFL team?",
+    answer_one: 8,
+    answer_two: 4,
+    answer_three: 2,
+    answer_four: 6,
+    correct_answer: '6'
+  },  
+
+];
+
+const user_response_correct = 
+`<div class="row">
+  <div class="col-12">
+    <div class="correct_answer">
+      <iframe src="https://giphy.com/embed/3o6Ztgb2DLyCHIgWYM" width=75% height=200px frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+      <p><a href="https://giphy.com/gifs/nfl-football-baltimore-ravens-3o6Ztgb2DLyCHIgWYM">via GIPHY</a></p>
+      <h2>Way to Go! Thats correct!</h2>
+      <form class="advance_question js-next-question">
+        <button type="submit" class="next_question js-next-question">Next question</button>
+      </form>
+    </div>
+  </div>
+</div>`;
---
+++ index.js
@@ -0,0 +1,198 @@
+'use strict';
+
+let question = 0;
+let score = 0;
+
+//This function will display feedback on the user's correct answer
+function displayFeedBackCorrect()
+{
+  $('.js-quiz-view').html(user_response_correct);
+  score++;
+  updateScoreDisplay();
+  advanceToNextQuestion();
+}
+
+//This function will display feedback on the user's incorrect answer
+function displayFeedBackIncorrect()
+{
+  let correctAnswer = 
+    `<div class="row">
+      <div class="col-12">
+        <div class="incorrect_answer">
+          <iframe src="https://giphy.com/embed/3o751Y4UC131goQSv6" width=75% height=200px frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+          <p><a href="https://giphy.com/gifs/nfl-football-baltimore-ravens-3o751Y4UC131goQSv6">via GIPHY</a></p>
+          <h2>Oh no! The correct answer is ${questionBank[question].correct_answer}
+          </h2>
+          <form class="advance_question js-next-question">
+          <button type="submit" class="next_question js-next-question">Next Question</button>
+          </form>
+        </div>
+      </div>
+    </div>`;
+
+  $('.js-quiz-view').html(correctAnswer);
+  advanceToNextQuestion();
+}
+
+//This function will check the user's answer
+function validateUserAnswer()
+{
+  $('.js-answer-form').submit(function(event)
+  {
+    //prevent default form submission
+    event.preventDefault();
+  
+    //Get the user response  
+    let userResponse = $('input[name="answer"]:checked').val();
+   
+    //validate the user response
+    if(questionBank[question].correct_answer === userResponse)
+    {
+      displayFeedBackCorrect();
+     
+     } else
+    {
+      displayFeedBackIncorrect();
+    }
+    
+  });
+}
+
+//Displays the new score when a response is correct.
+function updateScoreDisplay()
+{
+  $('.js-current-score').html(`${score}`);
+}
+
+//Updates the question display
+function updateQuestionDisplay()
+{
+  $('.js-question-count').html(`${question + 1}`);
+}
+
+//This function will display each question.
+function displayQuestion()
+{
+  updateQuestionDisplay();
+  
+  let currentQuestion =
+  `<div class="row">
+    <div class="col-12">
+      <h2>${questionBank[question].question}</h2>
+      <form class="answer_form js-answer-form">
+        <fieldset name="answer section">
+          <legend>Select one Answer</legend>
+            <label for="optionA" class="answer_one">
+              <input type="radio" id="optionA" name="answer" value="${questionBank[question].answer_one}" required autofocus>
+              <span>${questionBank[question].answer_one}</span>
+            </label>
+        
+            <label for="optionB" class="answer_two">
+              <input type="radio" id="optionB" name="answer" value="${questionBank[question].answer_two}" required>
+              <span>${questionBank[question].answer_two}</span>
+            </label>
+        
+            <label for="optionC" class="answer_three">
+              <input type="radio" id="optionC" name="answer" value="${questionBank[question].answer_three}" required>
+              <span>${questionBank[question].answer_three}</span>
+            </label>
+        
+            <label for="optionD" class="answer_four">
+              <input type="radio" id="optionD" name="answer" value="${questionBank[question].answer_four}" required>
+              <span>${questionBank[question].answer_four}</span>
+            </label>
+        
+            <button type="submit">Submit</button>
+          </fieldset>
+      </form>
+    </div>
+  </div>`;
+  
+  //render the current question.
+  $('.js-quiz-view').html(currentQuestion);
+  
+  //listen for the submit event
+  validateUserAnswer();
+  
+}
+
+//This function will display the quiz displayResults
+function displayResults()
+{
+  let quizResult = 
+  `<div class="row">
+    <div class="col-12">
+      <div class="results">
+        <img class="game_over" src="http://prod.static.ravens.clubs.nfl.com/assets/img/ravenstown/downloads/2015/Wallpapers_SuggsIntro_1024x768.jpg" alt="Raven Suggs Intro">
+        <h2>The hay is in the barn!</h2>
+        <h2>You answered ${score} questions correct!</h2>
+        <button type="submit" class="restart_quiz js-restart-quiz-button">Restart Quiz!</button>
+      </div>
+    </div>
+  </div>`;
+        
+  $('.js-quiz-view').html(quizResult);
+  
+  endQuizToRestart();
+  
+}
+
+//This function will advance to the next question.
+function advanceToNextQuestion()
+{
+  //increment the question counter.
+  question++;
+  
+  //check to see if all questions have been answered
+  $('.js-next-question').submit(function(events)
+    {
+      event.preventDefault();
+      
+      if(question === questionBank.length)
+      {
+        displayResults();
+      } else 
+      {
+        displayQuestion();
+      }
+      
+    });
+}
+
+//This function will start the quiz.
+function startQuiz()
+{
+  $('.js-quiz-launch').submit(function(event)
+  {
+    //prevent default form submission
+    event.preventDefault();
+    
+    //hide the start page section
+    $('.js-starting-view').empty();
+    
+    //display question 1
+    displayQuestion();
+  });
+  
+}
+
+//This function will end the quiz and allow the user to restart
+function endQuizToRestart()
+{
+  $('.js-restart-quiz-button').click(function(event)
+  {
+    location.reload(true);
+  });
+
+}
+
+//Main call back function for the quiz.
+//Will call all other functions on page load.
+function handleQuiz()
+{
+  startQuiz();
+  endQuizToRestart();
+}
+
+//Page Load call back function
+$(handleQuiz);
